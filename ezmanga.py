a0ecb1ed61a064d42565cb524390336008904ee0
Plextora <plextoraa.a@gmail.com>
2021-12-18 19:31:31 -0500

    Add arguments for ezmanga.py

--- ezmanga.py
+++ ezmanga.py
@@ -1,5 +1,8 @@
 import click
 from manga_down import mangareader
+import platform
+import getpass
+import os
 
 # import random shit
 
@@ -7,6 +10,29 @@ from manga_down import mangareader
 def CLI():
     pass # OwO
 
+operatingSystem = platform.system()
+user = getpass.getuser()
+
+if operatingSystem == "Linux":
+    defaultSave = r"/home/" + user + "/Downloads"
+if operatingSystem == "Windows":
+    ezMangaDirCheck = os.path.isdir(r"c:\\users\\{}\\AppData\Local\ezManga").format(os.getenv("username"))
+    if ezMangaDirCheck == False:
+        os.mkdir(r"c:\\users\\{}\\AppData\Local\ezManga")
+        defaultSave = r"c:\\users\\{}\\AppData\Local\ezManga".format(os.getenv("username"))
+    if ezMangaDirCheck == True:
+            efaultSave = r"c:\\users\\{}\\AppData\Local\ezManga".format(os.getenv("username"))
+if operatingSystem == "Darwin": # Darwin = OSX
+        defaultSave = r"/home/" + user + "/Downloads"
+
+@click.command()
+@click.option("--manga", help="Input name of the manga you want to download.")
+@click.option("--chapterlist", help="Returns the amount of chapters available for the manga you want to download.")
+@click.option("--chaptertodownload", help="The chapter number of the manga you want to download")
+@click.option("--saveto", help="Input location to download manga to.", default=defaultSave)
+
+def ezManga():
+    pass
 
 
 if __name__ == '__main__':
